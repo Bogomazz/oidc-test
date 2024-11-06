7efc38914913cf98e073fcc7c509695534258186
Anton Bohomaz <bogomaz97@gmail.com>
2024-11-06 16:35:33 +0100

    Add initial configuration for oidc provider

---
+++ index.js
@@ -0,0 +1,62 @@
+import Provider from 'oidc-provider'
+import NodeJose  from 'node-jose'
+import dotenv from 'dotenv'
+
+const {JWK} = NodeJose
+dotenv.config()
+const { 
+  SDO_PUBLIC_HOST: public_host, 
+  OIDC_CLIENT_ID: client_id, 
+  OIDC_REDIRECT_URIS: redirect_uris_str,
+  PORT,
+} = process.env
+const redirect_uris = redirect_uris_str?.split(',')
+
+const port = PORT || 8080;
+
+async function generateJWKS() {
+  const keystore = JWK.createKeyStore()
+  
+  const key = await keystore.generate('RSA', 2048, { alg: 'RS256', use: 'sig' })
+  
+  return {
+    keys: [key.toJSON(true)]
+  }
+}
+
+async function init() {
+
+  const jwks = await generateJWKS()
+
+  const configuration = {
+    clients: [
+      {
+        client_id,
+        client_secret: 'bar',
+        redirect_uris,
+        response_types: ['id_token'],
+        grant_types: ['implicit'],
+        token_endpoint_auth_method: 'none',
+        scope: 'openid',
+      },
+    ],
+    claims: {
+      openid: ['sub'],
+      email: ['email', 'email_verified'],
+      profile: ['name', 'family_name', 'given_name', 'middle_name', 'nickname', 'preferred_username', 'profile', 'picture', 'website', 'gender', 'birthdate', 'zoneinfo', 'locale', 'updated_at'],
+    },
+    issuer: `http://localhost:${port}/`,
+    jwks,
+  }
+  console.log(configuration.issuer)
+  
+  const oidc = new Provider(`http://0.0.0.0:${port}`, configuration)
+  
+  oidc.listen(port, () => {
+    console.log(
+      `oidc-provider listening on port ${port}, check http://${public_host}:${port}/.well-known/openid-configuration`,
+    )
+  })
+}
+
+init()
