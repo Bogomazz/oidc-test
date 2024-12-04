e9312b7395526fa401870401722eed707914dec2
Anton Bohomaz <bogomaz97@gmail.com>
2024-12-04 14:35:45 +0100

    read key and add to the keystore

--- generate-key.js
+++ generate-key.js
@@ -1,10 +1,19 @@
 import NodeJose  from 'node-jose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 const {JWK} = NodeJose
+const JWKS_KEY = process.env.JWKS_KEY
 
 async function generateJWKS() {
   const keystore = JWK.createKeyStore()
   
-  const key = await keystore.generate('RSA', 2048, { alg: 'RS256', use: 'sig' })
+  // const key = await keystore.generate('RSA', 2048, { alg: 'RS256', use: 'sig' })
+  const key = await keystore.add(
+    JSON.parse(JWKS_KEY)
+  )
+  console.log(key)
   console.log('PRIVATE KEY')
   console.log(
     JSON.stringify(
